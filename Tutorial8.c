83a22c15fe2f658d355c54a727f1e4b74b9544d7
rakshith <rakshithdogra@gmail.com>
2021-12-21 22:49:02 +0530

    problem 1 in tutorial 8 solved

---
+++ Tutorial8.c
@@ -0,0 +1,19 @@
+ # include <stdio.h>
+ // print the table of a given number till 10
+ 
+ int main(){
+     int e;
+     printf("Enter the required number: ");
+     scanf("%d", &e);
+     printf("%d X 1 = %d \n", e, e * 1);
+     printf("%d X 2 = %d \n", e, e * 2);
+     printf("%d X 3 = %d \n", e, e * 3);
+     printf("%d X 4 = %d \n", e, e * 4);
+     printf("%d X 5 = %d \n", e, e * 5);
+     printf("%d X 6 = %d \n", e, e * 6);
+     printf("%d X 7 = %d \n", e, e * 7);
+     printf("%d X 8 = %d \n", e, e * 8);
+     printf("%d X 9 = %d \n", e, e * 9);
+     printf("%d X 10 = %d \n", e, e * 10);
+     return 0;
+ }
