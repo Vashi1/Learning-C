d33a09d274400667666767b6e24ceca63cd1df23
rakshith <rakshithdogra@gmail.com>
2021-12-21 21:10:00 +0530

    V1

---
+++ add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    printf("Enter the number a: ");
+    scanf("%d", &a);
+    printf("Enter the number b: ");
+    scanf("%d", &b);
+    printf("Your added sum is %d", a+b);
+    return 0;
+}
