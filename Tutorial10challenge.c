aceb35c70e4684dcf5d5c2a6332bd92adc4cd94e
rakshith <rakshithdogra@gmail.com>
2021-12-23 08:23:18 +0530

    Tu1

---
+++ Tutorial10challenge.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+int main(){
+    int a;
+    int gift;
+    printf("Enter the exams you have passed(maths, science) : ");
+    scanf("%d", &a);
+    if (a == 2){
+        gift = 45;}
+    else {
+        gift = 15;
+    }
+    if (gift = 45){
+        printf("You have received a gift worth %d", &gift);
+    }
+    else{
+        printf("You have received a gift worth %d", &gift);
+    }
+    }
